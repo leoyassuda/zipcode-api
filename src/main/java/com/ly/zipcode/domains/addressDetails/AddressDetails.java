ec0632ff68ba1781af9c212a5086cf9c076c4b1e
Leo Yassuda <leo.yassuda@gmail.com>
2023-10-16 17:45:11 -0300

    update project version; add open-api docs and swagger; add messages validation;

--- src/main/java/com/ly/zipcode/domains/addressDetails/AddressDetails.java
+++ src/main/java/com/ly/zipcode/domains/addressDetails/AddressDetails.java
@@ -1,5 +1,6 @@
 package com.ly.zipcode.domains.addressDetails;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -21,24 +22,32 @@ public class AddressDetails implements Serializable {
   private UUID id;
 
   @Pattern(regexp = "\\d{5}-\\d{3}", message = "error.api.addressDetails.id.invalid")
+  @NotBlank(message = "addressDetails.zipcode.notEmpty")
   private String zipcode;
 
+  @NotBlank(message = "addressDetails.address.notEmpty")
   private String address;
 
   private String additionalAddress;
 
+  @NotBlank(message = "addressDetails.neighborhood.notEmpty")
   private String neighborhood;
 
+  @NotBlank(message = "addressDetails.city.notEmpty")
   private String city;
 
+  @NotBlank(message = "addressDetails.state.notEmpty")
   private String state;
 
+  @Pattern(regexp = "\\d{7}", message = "error.api.addressDetails.ibge.invalid")
   private String ibge;
 
+  @Pattern(regexp = "[A-Z0-9]+", message = "error.api.addressDetails.gia.invalid")
   private String gia;
 
+  @Pattern(regexp = "\\d{2}", message = "error.api.addressDetails.ddd.invalid")
   private String ddd;
 
+  @Pattern(regexp = "\\d{4}", message = "error.api.addressDetails.siafi.invalid")
   private String siafi;
-
 }
