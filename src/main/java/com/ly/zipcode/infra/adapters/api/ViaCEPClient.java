4727872d2ee66843059f045c7083baa696904aef
Leo Yassuda <leo.yassuda@gmail.com>
2023-10-17 13:00:01 -0300

    fix env files and build dockerfile

--- src/main/java/com/ly/zipcode/infra/adapters/api/ViaCEPClient.java
+++ src/main/java/com/ly/zipcode/infra/adapters/api/ViaCEPClient.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(name = "asd", url = "https://viacep.com.br/ws")
+@FeignClient(name = "viaCEP", url = "https://viacep.com.br/ws")
 public interface ViaCEPClient {
 
   @RequestMapping(method = RequestMethod.GET, value = "/{cep}/json", consumes = "application/json")
